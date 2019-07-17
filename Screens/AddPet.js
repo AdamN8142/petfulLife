0e9e5b012a906650807134b96fa7e05a2f99ca75
Adam Niedzwiecki <adamn8142@gmail.com>
2019-07-17 10:41:26 -0600

    Grab values for state
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>

--- Screens/AddPet.js
+++ Screens/AddPet.js
@@ -2,6 +2,7 @@ import React, { Component }from 'react';
 import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
 
 
+
 export class AddPet extends Component {
   constructor(){
     super();
@@ -12,25 +13,44 @@ export class AddPet extends Component {
       breed: '',
     } 
   }
+
+  // grabPetInfo = (idea) => {
+  //   let newIdea= this.state.text
+  //   this.setState({
+  //     concepts: [...this.state.concepts, newIdea]
+  //   })
+  // }
+
   render(){
+    console.log(this.state)
     return (
     <View style={styles.container}>
       <Text>Name</Text>
       <TextInput 
           style={styles.input}
+          onChangeText={(text)=> this.setState({name: text})}
+          value={this.state.name}
         />
       <Text>Nickname</Text>
       <TextInput 
           style={styles.input}
+          onChangeText={(text)=> this.setState({nickName: text})}
+          value={this.state.nickName}
         />
       <Text>Breed</Text>
       <TextInput 
           style={styles.input}
+          onChangeText={(text)=> this.setState({breed: text})}
+          value={this.state.breed}
         />
+        
+     
       <Button
         title="Submit!"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
+
+
       />
     </View>
     )
