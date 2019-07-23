ea9a292e36228cdf427366de48c86f87ab26b002
Lauren Boyer <lboyer4@outlook.com>
2019-07-23 08:50:52 -0600

    Added a picker still in progress

--- Screens/ViewProducts.js
+++ Screens/ViewProducts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Picker } from 'react-native';
 import { fetchData } from '../Utils/fetchCalls';
 
 export class ViewProducts extends Component {
@@ -8,17 +8,27 @@ export class ViewProducts extends Component {
 		this.state = {
 			name: 'null',
 			products: [],
+			pets: [],
+			error: ''
 		}
 	}
 
 	componentDidMount = (props) => {
-		const {navigation} = this.props
-		const pet = navigation.getParam('pet', 'null')
-		this.setPet(pet)
-		this.getProducts()
+		const {navigation} = this.props;
+		const pet = navigation.getParam('pet', 'null');
+		this.setPetName(pet);
+		this.getProducts();
+		this.getAllPets();
 	}
 
-	setPet = (pet) => {
+	getAllPets = () => {
+		let url = 'http://localhost:3000/api/v1/users/1/pets' 
+    fetchData(url)
+    .then(response => this.setState({ pets: response.data.attributes.pets}))
+    .catch(error => this.setState({error}))
+	}
+
+	setPetName = (pet) => {
 		this.setState({name: pet.name})
 	}
 
@@ -32,6 +42,17 @@ export class ViewProducts extends Component {
 		this.setState({products})
 	}
 
+	makePetPicker = (product) => {
+		console.log('does this product have pet id', product)
+		if (this.state.pets) {
+			return this.state.pets.map(pet => {
+				return (
+					<Picker.Item label={pet.name} key={pet.id} value={pet.name} />
+				)
+			})
+		}
+	}
+
 	makeProductProfiles = () => {
 		if (this.state.products.length) {
 			return this.state.products.map(product => {
@@ -39,15 +60,20 @@ export class ViewProducts extends Component {
 					<View key={product.id} style={styles.product}>
 						<Text>{product.name}</Text>
 						<Text>{product.avg_price}</Text>
+						<View>
+						<Picker style={{height: 2, mode: 'dropdown'}}>{this.makePetPicker(product)}</Picker>
+						</View>
 					</View>
 				)
 			})
+		} else {
+			return <Text>You have not scanned any products, yet</Text>
 		}
 	}
 
 	render() {
 		let greeting = this.state.name ? `${this.state.name}s products` : 'All Products'
-
+		console.log('state of pets in view products', this.state.pets)
 		return (
 			<ScrollView>
 				<View style={styles.container}>
