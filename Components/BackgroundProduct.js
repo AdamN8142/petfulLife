dc66a11de5d9970b6dbb6acbc922a08ffa6c7f19
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-07-24 17:03:19 -0600

    Attempts to style the view products screen
    Styles the page so that is able to scroll when multiple products are on the page
    Begins organization of the data on the page into cards currently places well on Android but places inconsistently on iOS
    Finds images for background pictures
    
    Co-authored-by: Jeremy Bennett <25406708+JaxJafinPapau@users.noreply.github.com>

---
+++ Components/BackgroundProduct.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import { StyleSheet, View, ImageBackground } from 'react-native';
+
+
+export class BackgroundProduct extends Component {
+  render() {
+    return(
+
+      <ImageBackground
+        source={require('../Images/PaintedPuppySpots.jpg')}
+        style={styles.container}
+        >
+      </ImageBackground>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    height:'100%',
+    width: '100%',
+    resizeMode: 'cover',
+    position:'absolute'
+  }
+});
